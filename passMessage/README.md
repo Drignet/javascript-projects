5bf423a79935008582df37211c566413e8b8670f
Drig <odibehenry@gmail.com>
2022-08-28 01:19:03 +0100

    project files

---
+++ passMessage/README.md
@@ -0,0 +1,6 @@
+# A simple pass message box
+## Practice how to grab and set an item from an input onto a div.
+
+* JS
+* HTML
+* CSS
---
+++ passMessage/script.js
@@ -0,0 +1,7 @@
+const input = document.querySelector('#message');
+const button = document.querySelector('button');
+const deliver = document.querySelector('h2');
+
+button.addEventListener('click', e => {
+    deliver.textContent += input.value;
+})

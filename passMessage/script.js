e0b450f7a18dcb5f98190008c320be939f67d191
Drig <odibehenry@gmail.com>
2022-08-28 01:36:03 +0100

    update

--- passMessage/script.js
+++ passMessage/script.js
@@ -3,5 +3,5 @@ const button = document.querySelector('button');
 const deliver = document.querySelector('h2');
 
 button.addEventListener('click', e => {
-    deliver.textContent += input.value;
+    deliver.textContent = input.value;
 })

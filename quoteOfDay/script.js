84fbf1c22c5b90f3289cfe45bcc464f5e3941bb9
Drig <odibehenry@gmail.com>
2022-08-31 20:13:17 +0100

    update

--- quoteOfDay/script.js
+++ quoteOfDay/script.js
@@ -10,7 +10,6 @@ async function quoteFunc(){
     let res = await fetch(url);
     let data = await res.json();
     let randNum = Math.floor(Math.random() * 1644);
-	quote.style.backgroundColor = '#C393A6';
     quote.textContent = data[randNum].text;
     localStorage.setItem('q', data[randNum].text);
     if(data[randNum].author === null){

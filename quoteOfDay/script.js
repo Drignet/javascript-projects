be93805845dacfc4d4153e54a77e86b7dd765f18
Drig <odibehenry@gmail.com>
2022-08-31 02:15:40 +0100

    updated

--- quoteOfDay/script.js
+++ quoteOfDay/script.js
@@ -2,7 +2,8 @@ const button = document.querySelector('button');
 const quote = document.querySelector('.quote-text');
 const author = document.querySelector('.author');
 
-
+quote.textContent = localStorage.getItem('q')
+author.textContent = localStorage.getItem('a')
 
 let url = 'https://type.fit/api/quotes'
 async function quoteFunc(){
@@ -10,7 +11,9 @@ async function quoteFunc(){
     let data = await res.json();
     let randNum = Math.floor(Math.random() * 1644);
     quote.textContent = data[randNum].text
+    localStorage.setItem('q', data[randNum].text)
     author.textContent = data[randNum].author
+    localStorage.setItem('a', data[randNum].author)
 }
 
 button.addEventListener('click', quoteFunc)

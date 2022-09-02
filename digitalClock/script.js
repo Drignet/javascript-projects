c4fb5a9647ecc3d0abc17ba0e1a641a4bcfaad3e
Drig <odibehenry@gmail.com>
2022-09-03 00:29:27 +0100

    update

--- digitalClock/script.js
+++ digitalClock/script.js
@@ -1,31 +1,39 @@
 const clock = document.querySelector('.clock');
+let timeSymbol = document.querySelector('.am-pm');
 
 setInterval(digitalClock, 1000)
-function digitalClock(){
+function digitalClock() {
     let now = new Date();
     let hours = now.getHours();
     let minutes = now.getMinutes();
     let seconds = now.getSeconds();
     let ourTime;
-    if(hours < 10){
+
+    const amPm = [' AM', ' PM'];
+
+    if (hours < 10) {
         hours = `${'0' + hours}`;
     }
-    if(minutes < 10){
+    if (minutes < 10) {
         minutes = `${'0' + minutes}`;
     }
-    if(seconds < 10){
+    if (seconds < 10) {
         seconds = `${'0' + seconds}`;
     }
 
-    if(hours >= 12 && hours <24){
-        hours = hours - 12;
-    ourTime = `${hours}:${minutes}:${seconds} PM`;
-    }else if(hours === 24){
-        hours = hours - 12;
-            ourTime = `${hours}:${minutes}:${seconds} AM`;
-    }
-    else{
-        ourTime = `${hours}:${minutes}:${seconds} AM`;
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+
+    ourTime = `${hours}:${minutes}:${seconds}`;
+    clock.childNodes[0].textContent = ourTime;
+
+    (hours > 12) ? clock.childNodes[1].textContent = amPm[1] : clock.childNodes[1].textContent = amPm[0];
 }
-    clock.textContent = ourTime;
+
+setInterval(color, 3000)
+function color() {
+    let randomColor = '#' + (Math.random() * 10).toString(16).slice(2, 8);
+    timeSymbol.style.color = randomColor;
 }
+
+

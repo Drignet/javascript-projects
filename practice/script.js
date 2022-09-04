92f4f07f2fac4c23b3672d32dac4d8ba7b17f254
drignet <odibehenry@gmail.com>
2022-09-04 16:45:30 +0100

    update

--- practice/script.js
+++ practice/script.js
@@ -11,10 +11,9 @@ btns.forEach((btn) => {
         e.preventDefault;
     const ele = e.currentTarget.classList
     ele.contains('increase') ? count++ : ele.contains('decrease') ? count-- : count = 0;
+
+result.textContent = count;                             count > 0 ? result.style.color = 'green' : count < 0 ? result.style.color = 'red' : result.style.color = 'go    ld';
     })
 })
-
-result.textContent = count;                             count > 0 ? result.style.color = 'green' : count < 0 ? result.style.color = 'red' : result.style.color = 'go
-ld';
 
 localStorage.setItem('number' , count)

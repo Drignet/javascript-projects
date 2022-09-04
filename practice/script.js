5900c413873546f79646b6aa07187b0dcde61ada
drignet <odibehenry@gmail.com>
2022-09-04 16:51:31 +0100

    update

--- practice/script.js
+++ practice/script.js
@@ -9,11 +9,12 @@ result.textContent = localStorage.getItem('number');
 btns.forEach((btn) => {
     btn.addEventListener('click', e => {
         e.preventDefault;
+result.textContent = localStorage.getItem('number');
     const ele = e.currentTarget.classList
     ele.contains('increase') ? count++ : ele.contains('decrease') ? count-- : count = 0;
 
 result.textContent = count;                             count > 0 ? result.style.color = 'green' : count < 0 ? result.style.color = 'red' : result.style.color = 'go    ld';
+
+localStorage.setItem('number' , count);
     })
 })
-
-localStorage.setItem('number' , count)

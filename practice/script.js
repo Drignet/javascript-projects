3db7f81f7aa0fe06e3bdcbd7ecd9ef055d40fdc2
Drig <odibehenry@gmail.com>
2022-09-04 15:03:45 +0100

    update

---
+++ practice/script.js
@@ -0,0 +1,16 @@
+const result = document.querySelector('span');
+const btns = document.querySelectorAll('.btns');
+
+result.textContent = localStorage.getItem('number');
+let count = 0
+btns.forEach((btn) => {
+    btn.addEventListener('click', e => {
+        e.preventDefault;
+    const ele = e.currentTarget.classList
+    ele.contains('increase') ? count++ : ele.contains('decrease') ? count-- : count = 0;
+    
+    result.textContent = count;
+    count > 0 ? result.style.color = 'green' : count < 0 ? result.style.color = 'red' : result.style.color = 'gold';
+    localStorage.setItem('number' , count)
+    })
+})

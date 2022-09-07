3cc8fb4ad9d34f5fb7c748026bef5d73a21d6cf0
Drig <odibehenry@gmail.com>
2022-09-07 01:55:42 +0100

    update

--- tipCalc/script.js
+++ tipCalc/script.js
@@ -16,9 +16,15 @@ btns.forEach((btn) => {
             results[i].textContent = '';
             }
         }
-        else if (style.contains('calculate') && (bill.value === '') || (tip.value === '') || people.value === ''){
-            alert('dont leave it empty');
+        else if (style.contains('calculate') && (+bill.value === '') || (+tip.value === '') || people.value === ''){
+            alert('Dont leave your values empty');
             }
+        else if (+bill.value <=0 || +people.value <= 0){
+            alert('Please Re-confirm your bill / Number of people')
+        }
+        else if (+people.value !== Math.floor(+people.value)){
+            alert('we dont have decimal human')
+        }
         else {
                 const tipAmount = Math.ceil(((+tip.value / 100) * +bill.value));
                 const totalAmount = (+bill.value + tipAmount);

7401c8fa7ce14c30260e38d61139c8ec6aaf3fb4
Drig <odibehenry@gmail.com>
2022-09-07 14:07:29 +0100

    update

--- dayOfWeek/script.js
+++ dayOfWeek/script.js
@@ -1,5 +1,11 @@
 const day = document.querySelector('p');
 
+const now = new Date();
+const today = now.getDay();
+
+const days = ['Sunday' , 'Monday' , 'Tuesday' , 'Wednesday' , 'Thursday' , 'Friday' , 'Saturday'];
+day.textContent = days[today];
+
 setInterval(randomColor, 1000);
 function randomColor(){
     let randCol = '#' + (Math.random() * 10).toString(16).slice(2, 8);

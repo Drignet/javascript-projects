2ebc102ad482047ec829b5622ff49afd3858914f
Drig <odibehenry@gmail.com>
2022-09-09 01:33:37 +0100

    update

---
+++ additionGame/README.md
@@ -0,0 +1,7 @@
+# Addition Game Project
+## Get correct answer by adding two numbers
+
+### A javascript projectdone by application of :
+* Random function
+* Event listener
+
---
+++ additionGame/script.js
@@ -0,0 +1,38 @@
+const nums = document.querySelectorAll('.num');
+const ans = document.querySelector('#ans');
+const check = document.querySelector('button');
+const correct = document.querySelector('.correct');
+const inCorrect = document.querySelector('.incorrect');
+
+
+let correctScore = 0;
+let incorrectScore = 0;
+
+
+check.addEventListener('click', e=> {
+    let num1 = +nums[0].textContent;
+    let num2 = +nums[1].textContent;
+    let answer = (num1 + num2);
+
+    if (answer === +ans.value){
+        correctScore++;
+        correct.childNodes[1].textContent = correctScore;
+    }
+    else{
+        incorrectScore++;
+        inCorrect.childNodes[1].textContent = incorrectScore;
+        inCorrect.childNodes[1].style.color = 'red';
+    }
+
+    nums[0].textContent = '';
+    nums[1].textContent = '';
+    ans.value = '';
+
+    nums[0].textContent = randomNumber();
+    nums[1].textContent = randomNumber();
+})
+
+function randomNumber(){
+    return (Math.floor(Math.random() * 10));
+}
+

df4e543fd3a1d93c60015bee221564250a82cf65
Drig <odibehenry@gmail.com>
2022-09-18 21:01:58 +0100

    update

--- navbarDark/script.js
+++ navbarDark/script.js
@@ -2,7 +2,6 @@ const mobileMenu = document.querySelector('.mobile-menu');
 const mainMenus = document.querySelector('.main-menus');
 mobileMenu.addEventListener('click', e => {
     mainMenus.classList.toggle('show-main-menu');
-    mainMenus.style.transform = 'translateY(8.5em)';
     
 
 })

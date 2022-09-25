3f58b9d857eb65b3e0901759e419dcfda5bcf2eb
Drig <odibehenry@gmail.com>
2022-09-25 12:09:56 +0100

    update

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,6 +1,31 @@
-const mobileMenu = document.querySelector('.menus');
+const header = document.querySelector('header');
+const wrapper = document.querySelector('.wrapper');
+const btnDarkMode = header.querySelector('.circle');
 const menuHandle = document.querySelector('.menu-handle');
 
-menuHandle.addEventListener('click' , (e) => {
+const contactSocial = document.querySelector('.contact-socials');
+const share = document.querySelector('.share');
+
+
+
+menuHandle.addEventListener('click' , () => {
+    const mobileMenu = header.querySelector('.menus');
     mobileMenu.classList.toggle('show')
 })
+
+btnDarkMode.addEventListener('click' , () => {
+    const darkBtn = header.querySelector('.dark-btn');
+    darkBtn.classList.toggle('dark-mode');
+    wrapper.classList.toggle('d');
+})
+
+share.addEventListener('mouseover' , () => {
+    
+        contactSocial.classList.toggle('show-social');
+
+    setTimeout(() => {
+        contactSocial.classList.remove('show-social');
+    }, 10000)
+    
+})
+
